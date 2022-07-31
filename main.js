7469af7ff71f90a9366b4d3c75560fc3e81b1ec5
Veydem <korsarishe@gmail.com>
2022-07-31 16:29:13 +0300

    update

--- main.js
+++ main.js
@@ -1,30 +1,28 @@
-// All answer options
+// Все варианты ответов
 const option1 = document.querySelector('.option1'),
       option2 = document.querySelector('.option2'),
       option3 = document.querySelector('.option3'),
       option4 = document.querySelector('.option4');
 
-// All our options
-// console.log(optionElements);
+// Все наши ответы
 const optionElements = document.querySelectorAll('.option');
 
-const question = document.getElementById('question'); //question
+const question = document.getElementById('question'); // сам вопрос
 
-const NumberOfQuestion = document.getElementById('number-of-question'), // number Of question
-    NumberOfAllQuestions = document.getElementById('number-of-all-questions'); // number of all questions
+const numberOfQuestion = document.getElementById('number-of-question'), // номер вопроса
+      numberOfAllQuestions = document.getElementById('number-of-all-questions'); // количество всех вопросов
 
-let indexOfQuestion, // current question index
-    indexOfPage = 0; // page index
+let indexOfQuestion, // индекс текущего вопроса
+    indexOfPage = 0; // страница индекса
 
-const answersTracker = document.getElementById('answers-tracker'); // tracker wrapper
+const answersTracker = document.getElementById('answers-tracker'); // обертка для трекера
+const btnNext = document.getElementById('btn-next'); // кнопка далее
 
-const btnNext = document.getElementById('btn-next'); // button next
+let score = 0; // итоговый результат викторины
 
-let score = 0; // final result
-
-const correctAnswer = document.getElementById('correct-answer'), // number of correct answers
-    NumberOfAllQuestions2 = document.getElementById('number-of-all-questions-2'), // number of correct answers (in the modal window)
-    btnTryAgain = document.getElementById('btn-try-again'); // start again button
+const correctAnswer = document.getElementById('correct-answer'), // количество правильных ответов
+    numberOfAllQuestions2 = document.getElementById('number-of-all-questions-2'), // количество правильных ответов (в модальном окне)
+    btnTryAgain = document.getElementById('btn-try-again'); // кнопка повторного запуска
 
 const questions = [
     {
@@ -56,32 +54,122 @@ const questions = [
             'Плохо',
         ],
         rightAnswer: 3
-    },
+    }
 ];
 
-NumberOfAllQuestions.innerHTML = questions.length; // output the number of questions
+numberOfAllQuestions.innerHTML = questions.length; // вывод количества вопросов
 
 const load = () => {
-    question.innerHTML = questions[0].question; // question
+    question.innerHTML = questions[indexOfQuestion].question; // сам вопрос
 
-    // mapping all the answers
+    // мапинг всех ответов
     option1.innerHTML = questions[indexOfQuestion].options[0];
     option2.innerHTML = questions[indexOfQuestion].options[1];
     option3.innerHTML = questions[indexOfQuestion].options[2];
     option4.innerHTML = questions[indexOfQuestion].options[3];
 
-    NumberOfQuestion.innerHTML = indexOfPage + 1; // setting the current page number
-    indexOfPage++; //page index increase
+    numberOfQuestion.innerHTML = indexOfPage + 1; // установка номера текущей страницы
+    indexOfPage++; //увеличение индекса страницы
 };
 
-let completedAnswers = [] // array for questions already asked
+let completedAnswers = []; // массив для уже заданных вопросов
 
 const randomQuestion = () => {
     let randomNumber = Math.floor(Math.random() * questions.length);
+    let hitDuplicate = false; // якорь для проверки тех же вопросов
+
+    if(indexOfPage == questions.length) {
+        quizOver()
+    } else {
+        if(completedAnswers.length > 0) {
+            completedAnswers.forEach(item => {
+                if(item == randomNumber) {
+                    hitDuplicate = true;
+                }
+            });
+            if(hitDuplicate) {
+                randomQuestion();
+            }else {
+                indexOfQuestion = randomNumber;
+                load();
+            }
+        }
+        if(completedAnswers.length == 0) {
+            indexOfQuestion = randomNumber;
+            load(); 
+        }
+    }
+    completedAnswers.push(indexOfQuestion);
+};
+
+const checkAnswer = el => {
+    if(el.target.dataset.id == questions[indexOfQuestion].rightAnswer) {
+        el.target.classList.add('correct');
+        updateAnswerTracker('correct');
+        score++;
+    } else {
+        el.target.classList.add('wrong');
+        updateAnswerTracker('wrong');
+    }
+    disabledOptions();
 }
 
-    randomQuestion();
+for(option of optionElements) {
+    option.addEventListener('click' ,  e => checkAnswer(e));
+}
+
+const disabledOptions = () => {
+    optionElements.forEach(item => {
+        item.classList.add('disabled');
+        if(item.dataset.id == questions[indexOfQuestion].rightAnswer) {
+            item.classList.add('correct');
+        }
+    })
+}
+// удаление всех классов со всех ответов
+const enableOptions = () => {
+    optionElements.forEach(item => {
+        item.classList.remove('disabled', 'correct', 'wrong');
+    })
+};
+
+const answerTracker = () => {
+    questions.forEach(() => {
+        const div = document.createElement('div');
+        answersTracker.appendChild(div);
+    })
+};
+
+const updateAnswerTracker = status => {
+    answersTracker.children[indexOfPage - 1].classList.add(`${status}`);
+}
+
+const validate = () => {
+    if(!optionElements[0].classList.contains('disabled')) {
+        alert('Вам нужно выбрать один из вариантов ответа');
+    } else {
+        randomQuestion();
+        enableOptions();
+    }
+}
+
+const quizOver = () => {
+    document.querySelector('.quiz-over-modal').classList.add('active');
+    correctAnswer.innerHTML = score;
+    numberOfAllQuestions2.innerHTML = questions.length;
+};
+
+const tryAgain = () => {
+    window.location.reload();
+}
+
+btnTryAgain.addEventListener('click', tryAgain);
+
+btnNext.addEventListener('click', () => {
+    validate();
+})
 
 window.addEventListener('load', () => {
-    load();
-})
+    randomQuestion();
+    answerTracker();
+});
